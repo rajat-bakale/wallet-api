ddab60ebf6112781f03e79b76218fbee13d02b87
rajat-bakale <105482719+rajat-bakale@users.noreply.github.com>
2024-07-18 11:46:49 +0530

    Update transactionRoutes.js

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { transfer, transactionHistory, allTransactions } from '../controllers/transactionController.js';
 import checkAuth from '../middlewares/authMiddleware.js';
-import checkAdmin from '../middleware/adminCheck.js';
+import adminCheck from '../middleware/adminCheck.js';
 
 const router = express.Router();
 
 router.post('/transfer', transfer);
 router.get('/history', checkAuth, transactionHistory);
-router.get('/all', checkAuth, checkAdmin, allTransactions);
+router.get('/all', checkAuth, adminCheck, allTransactions);
 
 export default router;
